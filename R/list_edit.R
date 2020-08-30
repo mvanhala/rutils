0445149f8b31220d1a842de80a857abce59e18c1
Matthew Van Hala <mvanhala@users.noreply.github.com>
2020-08-30 01:09:45 -0700

    add list_edit function

---
+++ R/list_edit.R
@@ -0,0 +1,30 @@
+#' Edit a list
+#'
+#' Edit the elements of a list by name
+#'
+#' `list_edit` is similar to [purrr::list_modify] in
+#' `purrr`, but unlike `list_modify`, does not operate recursively
+#' on a list. It only edits elements at the top level of hierarchy.
+#' Elements can be removed by setting them equal to `NULL`.
+#'
+#' @param .l List to edit
+#' @param ... Named elements to add or delete
+#'
+#' @examples
+#' list_edit(list(x = 1, y = 2), x = 10, z = 20)
+#' list_edit(list(x = 1, y = 2), x = NULL, y = 5)
+#' list_edit(list(x = 1, y = 2), !!!list(x = NULL, z = 100))
+#' @export
+list_edit <- function(.l, ...) {
+  checkmate::assert_list(.l)
+  x <- .l
+  y <- rlang::list2(...)
+  for (nm in intersect(names(x), names(y))) {
+    x[[nm]] <- y[[nm]]
+  }
+  for (nm in setdiff(names(y), names(x))) {
+    x[[nm]] <- y[[nm]]
+  }
+  x
+}
+
