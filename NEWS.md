08f435030456fe36f143e1906748689430cea9fd
Matthew Van Hala <mvanhala@users.noreply.github.com>
2023-02-02 19:21:14 -0800

    add argument in list_edit() (#3)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# rutils 0.3.1
+
+* Add an argument to `list_edit()` for whether new elements should be added to the list.
+
 # rutils 0.3.0
 
 * Enable support for rendering to multiple formats in `render_doc`.
--- R/list_edit.R
+++ R/list_edit.R
@@ -9,21 +9,26 @@
 #'
 #' @param .l List to edit
 #' @param ... Named elements to add or delete
+#' @param .add Should named elements not existing in the original list be added?
 #'
 #' @examples
 #' list_edit(list(x = 1, y = 2), x = 10, z = 20)
 #' list_edit(list(x = 1, y = 2), x = NULL, y = 5)
 #' list_edit(list(x = 1, y = 2), !!!list(x = NULL, z = 100))
+#' list_edit(list(x = 1, y = 2), x = 10, z = 20, .add = FALSE)
 #' @export
-list_edit <- function(.l, ...) {
+list_edit <- function(.l, ..., .add = TRUE) {
   checkmate::assert_list(.l)
+  checkmate::assert_flag(.add)
   x <- .l
   y <- rlang::list2(...)
   for (nm in intersect(names(x), names(y))) {
     x[[nm]] <- y[[nm]]
   }
-  for (nm in setdiff(names(y), names(x))) {
-    x[[nm]] <- y[[nm]]
+  if (.add) {
+    for (nm in setdiff(names(y), names(x))) {
+      x[[nm]] <- y[[nm]]
+    }
   }
   x
 }
